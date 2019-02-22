815882b731f2dc920ae825eeba14c4a0499d5547
Thais Moraes <murici.thais@gmail.com>
2019-02-22 12:19:29 -0300

    Avoiding access to login and registration routes by logged users

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,22 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/', SessionController.create)
+// Sign in
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+// Registration
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+// Every '/app' route will be accessible only by logged users
+routes.use('/app', authMiddleware)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
